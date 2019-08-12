3f194ab13eb31f183ca187165f86300f5072bbbe
Taci Ata Küçükpınar <taci.ata72@gmail.com>
2019-08-12 23:16:13 +0300

    Added Cactus, Lizard, Trophy animations and Spawner

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    float walkSpeed = 0.69f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+    }
+
+    public void SetMovementSpeed(float speed)
+    {
+        walkSpeed = speed;
+    }
+}
