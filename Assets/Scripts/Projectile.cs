d946b67ce4caa020ba237886ba092c4c12253bd4
Taci Ata Küçükpınar <taci.ata72@gmail.com>
2019-08-13 15:00:03 +0300

    Made projectile move, damage and trigger vfx

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [Range(0f, 10f)] [SerializeField] float projectileSpeed = 2f;
+    [SerializeField] float projectileDamage = 50f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * projectileSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        var health = col.GetComponent<Health>();
+        var attacker = col.GetComponent<Attacker>();
+        
+        if(attacker && health)
+        {
+            health.DealDamage(projectileDamage);
+            Destroy(gameObject);
+        }
+        
+    }
+    
+
+}
