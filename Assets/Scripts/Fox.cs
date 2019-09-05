8a4875d614c24b34fb377ae9ccb067a65655c6a9
Taci Ata Küçükpınar <taci.ata72@gmail.com>
2019-09-05 12:20:01 +0300

    Added Fox

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        GameObject otherObject = collider.gameObject;
+
+        if (collider.tag == "Gravestone")
+        {
+            GetComponent<Animator>().SetTrigger("Jump Trigger");
+        }
+
+        else if (otherObject.GetComponent<Defender>())
+        {
+            GetComponent<Attacker>().Attack(otherObject);
+        }
+    }
+}
